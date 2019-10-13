02d47b1af816c84dad3ec7bb41e76ebefb72064d
Ravshan <ravkart@gmail.com>
2019-10-12 22:14:20 -0700

    updated signin and signup components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
 import Header from './components/header/header.component'
-import HomePage from '/pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import './App.css';
 
 class App extends Component {
